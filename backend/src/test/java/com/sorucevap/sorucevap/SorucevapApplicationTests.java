aba2038372ab99ef6a08e6bf486483d8476b75f7
minervain <aliosmanucar987@gmail.com>
2023-09-23 23:39:24 +0300

    1-commit

---
+++ backend/src/test/java/com/sorucevap/sorucevap/SorucevapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorucevap.sorucevap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorucevapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
