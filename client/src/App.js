e675424959053fba1786d2034ee6e6ef30acec26
minervain <aliosmanucar987@gmail.com>
2023-09-30 22:17:59 +0300

    -*-

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
 import Post from "./Components/Post";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/home";
+import User from "./Components/User/User";
+import Navbar from "./Components/Navbar/Navbar";
+
 function App() {
   return (
     <div className="App">
-     <Post/>
+      <BrowserRouter >  
+      
+      <Navbar/>
+            <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users/:userId" element={<User />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/Components/Post/index.js
+++ client/src/Components/Post/index.js
@@ -1,42 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from "react";
+import "./Post.scss";
+function Post(props) {
+   const {title, text} = props;
 
-function Post() {
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [postData, setPostData] = useState([]);
-
-    useEffect(() => {
-        fetch("/posts")
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    setLoading(true);
-                    setPostData(result);
-                },
-                (error) => {
-                    setLoading(true);
-                    setError(error);
-                }
-            );
-    }, []);
-
-    if (error)
-        return <div>error</div>;
-
-    return (
-        <div>
-            {loading ? (
-                postData.map((post) => (
-                    <div key={post.id}>
-                        <h2>{post.title}</h2>
-                        <p>{post.text}</p>
-                    </div>
-                ))
-            ) : (
-                <div>Loading...</div>
-            )}
-        </div>
-    );
+   return(
+       <div className="postContainer">
+           {title}
+           {text}
+       </div>
+   )
 }
 
 export default Post;
